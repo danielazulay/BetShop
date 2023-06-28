7e8243279e21b0c42198d96c3428f44625533f67
danielazulay <danielazulay20@gmail.com>
2023-06-28 12:19:25 +0300

    frontend first

---
+++ frontend/src/screens/Home.js
@@ -0,0 +1,22 @@
+import {Row,Col}from 'react-bootstrap'
+import products from '../products/products'
+import Product from '../components/Product'
+const Home = () => {
+  return (
+    <>
+        <h1>Latest Products</h1>
+        <Row>
+            {products.map((product)=>{
+                return(
+                    <Col key={product._id} sm={12} md={6} xl={3}>
+                    <Product product={product}/>
+                    </Col>
+                )
+
+            })}
+        </Row>
+    </>
+  )
+}
+
+export default Home
